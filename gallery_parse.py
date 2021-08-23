1f29690e70af550138315f8ca4fc812197076290
Artem <usualtom@gmail.com>
2021-08-24 00:42:36 +0300

    parsing image href and title pic

--- gallery_parse.py
+++ gallery_parse.py
@@ -2,12 +2,38 @@ from bs4 import BeautifulSoup
 import requests
 from fake_useragent import UserAgent
 
-#Url site: Gallerix
+#url site: Gallerix.ru
 url = "https://gallerix.ru/album/200-Russian/"
 
+#server bypass
 ua = UserAgent()
 
-print(ua.chrome)
+#copy webpage
+response = requests.get(url, headers={'User-Agent': ua.chrome})
+html = response.content
+soup = BeautifulSoup(html, "lxml")
 
-response = requests.get(url, headers={'User-Agent':ua.chrome})
+#start parse
+print(soup.head.title.text[:46])
+
+#create database with name and picture
+all_urls = []
+
+for obj in soup.findAll(attrs={'class': 'pic'}):
+    for urls in obj:
+        if urls.find(attrs={'class': 'a_dv1'}):
+            all_urls.append(urls.attrs['href'])
+
+
+href_img = []
+name_img = []
+
+for url in all_urls:
+    response = requests.get("https://gallerix.ru" + url, headers={'User-Agent': ua.chrome})
+    html = response.content
+    soup = BeautifulSoup(html, 'lxml')
+    href_img.append(soup.find(attrs={'id': 'xpic'}).attrs['src'])
+    name_img.append(soup.find(attrs={'id': 'axpic'}).attrs['title'][:soup.find(attrs={'id': 'axpic'}).attrs['title'].find(".")])
+
+
 
